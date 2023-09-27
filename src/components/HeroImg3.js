942cf9346f09cb63c9da1468684f002dde78d982
unknown <mailamaynayak@gmail.com>
2023-09-27 14:31:45 +0530

    finally

--- src/components/HeroImg3.js
+++ src/components/HeroImg3.js
@@ -9,7 +9,7 @@ class HeroImg3 extends Component {
         <h1>{this.props.heading}</h1>
         <div className="paragraph-container">
       <p>{this.props.text}</p>
-      <p className='mobile-paragraph'>Byte Craft Labs is a dynamic and innovative company at the forefront of cutting-edge technology. Our expertise spans a wide spectrum, from Cloud Computing with GCP and AWS to crafting Web 3 and Blockchain applications. We're the architects behind top-notch Android Apps, pioneers in AI/ML solutions, and masters of Python programming. Additionally, our Web Development team excels in creating Web 2 experiences that redefine the digital landscape. At Byte Craft Labs, we transform ideas into reality,</p>
+      <p className='mobile-paragraph'>Byte Craft Labs is a dynamic and innovative company at the forefront of cutting-edge technology.</p>
     </div>
     {/* <div className="button-container">
           <Link to={""}>
